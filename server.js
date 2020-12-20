0d929218f0472bea0d64b871ef9ea2689b261eec
StefanVelkoski <velkoskis@gmail.com>
2020-12-20 19:48:21 +0100

    cors

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+// import cors from 'cors';
+
+app.use(cors());
+
 app.use(express.static(__dirname + '/dist/oddalecise'));
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname + '/dist/oddalecise/index.html'));
