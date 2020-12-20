14043b09a28c88b06ee8f9e6bff8bcdd92218a6b
StefanVelkoski <velkoskis@gmail.com>
2020-12-20 19:55:30 +0100

    cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 // import cors from 'cors';
 
-app.use(cors());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.static(__dirname + '/dist/oddalecise'));
 app.get('/*', function(req,res){
